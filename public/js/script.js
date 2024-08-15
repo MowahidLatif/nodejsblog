75f3974f4887f63fd04a1d980bde84e5de1380b2
mowahid latif <mowahidlatif@hotmail.com>
2024-08-14 20:37:12 -0500

    Some Routes, Some Images and Some SCSS Completed

---
+++ public/js/script.js
@@ -0,0 +1,1 @@
+// alert('dslkfjasdkljfasd;klfjhadsk')
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,8 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('', (req, res) => {
-    res.send('Shut the hell up bro...');
-})
+router.get("/", (req, res) => {
+  const local = {
+    title: "Home Page",
+    description: "Welcome to my home page",
+  };
+  res.render("index", { local });
+});
+
+router.get("/contact", (req, res) => {
+  const local = {
+    title: "Contact Page",
+    description: "Contact me at my email",
+  };
+
+  res.render("contact", { local });
+});
+
+router.get("/about", (req, res) => {
+  const local = {
+    title: "About Page",
+    description: "About Me!",
+  };
+  res.render("about", { local });
+});
 
 module.exports = router;
