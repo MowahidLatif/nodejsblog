476b0ede94461f17dd445176f57efefef8c93e83
mowahid latif <mowahidlatif@hotmail.com>
2024-08-17 16:06:08 -0500

    Added Data to Collection and created Schema

---
+++ server/models/car.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const carSchema = new Schema({
+  make: {
+    type: String,
+    required: true,
+  },
+  model: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  mileage: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Car", carSchema);
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const Post = require("../models/post");
+const CarData = require("../models/car");
 
-router.get("/", (req, res) => {
+router.get("", async (req, res) => {
   const local = {
     title: "Home Page",
     description: "Welcome to my home page",
   };
-  res.render("index", { local });
+
+  try {
+    const data = await Post.find();
+    res.render("index", { local, data });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.get("/contact", (req, res) => {
@@ -33,5 +41,26 @@ router.get("/careers", (req, res) => {
   };
   res.render("careers", { local });
 });
+
+// function insertCarDataTest() {
+//   CarData.insertMany([
+//     { make: "Lamborghini", model: "Aventador", year: 2022, mileage: 1500 },
+//     { make: "Ferrari", model: "488 GTB", year: 2021, mileage: 5000 },
+//     { make: "Porsche", model: "911 Turbo S", year: 2023, mileage: 1200 },
+//   ]);
+// }
+
+// insertCarDataTest();
+
+// function insertPostData () {
+//   Post.insertMany([
+//     {
+//       title: "Building APIs with Node.js",
+//       body: "Learn how to use Node.js to build RESTful APIs using frameworks like Express.js"
+//     }
+//   ])
+// }
+
+// insertPostData();
 
 module.exports = router;
