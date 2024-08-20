de295bc7fba34592b4401c48c48d5e4b90e65ea5
mowahid latif <mowahidlatif@hotmail.com>
2024-08-19 19:04:01 -0500

    Created Auth and Added token check

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
+const cookiesParser = require("cookie-parser");
+const session = require('express-session');
+const MongoStore = require("connect-mongo");
 
 const connectDB = require("./server/config/db");
 
@@ -11,6 +14,17 @@ const PORT = 5005 || process.env.PORT;
 connectDB();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookiesParser());
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+  })
+);
 
 app.use(express.static("public"));
 
@@ -18,8 +32,8 @@ app.use(expressLayout);
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
-app.use('/', require('./server/routes/main'));
-app.use('/', require('./server/routes/admin'));
+app.use("/", require("./server/routes/main"));
+app.use("/", require("./server/routes/admin"));
 
 app.listen(PORT, () => {
   console.log("App listening on port: ", PORT);
